1c5f971d9f22e5ede65f6faa6fac0cea4ec38936
Arty Khvediouk <arty.khvediouk@okta.com>
2022-11-15 11:20:42 -0800

    Creating teh repo

--- README.md
+++ README.md
@@ -1,8 +1,150 @@
 
-Welcome to your new repo, okta-ocsf-syslog!
 
-This is a default readme file created to provide some helpful links and first steps.
-A lot of your questions might already exist in our [wiki page](http://bit.ly/EngFAQ), so please check there first.
-Your new artifact is available on bacon [here](https://bacon-go.aue1e.saasure.net/commits?artifact=okta-ocsf-syslog)
+## Architecture  
 
-Please reach out to us over on [#eng-release](https://okta.slack.com/archives/C7L27G2Q5) or [#eng-productivity](https://okta.slack.com/archives/C7LQ4U8T0) for any additional assistance you might require.
+Here’s how to convert incoming Okta System Log JSON data using the AWS Lambda function. Use the format conversion feature of Kinesis Firehose to convert the JSON data into Parquet. 
+
+ ![image](https://user-images.githubusercontent.com/2838125/202000596-978d784c-23d2-474a-b83a-8d14d8527fbf.png)
+
+
+<b>Step 1</b>: Create integration between Okta and Amazon EventBridge
+
+<b>Step 2 </b>: Define an event rule filter to capture events from Okta System Log and communicate it to Amazon Kinesis Firehose
+
+
+<b>Step 3 </b>: Firehose stream invokes a Lambda as it receives an event from EventBridge. The Lambda function will transform Okta’s System Log data into OCSF-formatted JSON.
+
+
+<b>Step 4 </b> : Configure a Firehose data stream to transform Okta System Log into OCSF format by invoking a Lambda function.
+
+
+<b>Step 5 </b> : Configure a Firehose data stream to convert the OCSF format from step 4 into a parquet record. (Parquet is the only acceptable format for Amazon Security Lake)
+
+
+<b>Step 6 </b> : Converted Parquet files with OCSF schema will be stored in an S3 bucket as part of Amazon Security Lake. 
+		 	 	 		
+
+## FAQs
+  
+###  Why is this integration guide helpful?
+
+This integration guide provides an automated approach to access Syslogs outside Okta and convert them into an OCSF format for any downstream analytical applications. Using the architecture explained in this post, customers can convert User Assignment/Un-Assignment,  Application Assignment/Un-Assignments, and MFA enrollment log events can be captured and normalized to OCSF schema format.  
+
+### How to use this integration guide?
+
+This integration guide is for customers, data engineers, and developers who want to convert Okta’s Syslogs into OCSF schema format. The architecture pattern in this integration guide will help customers build a data pipeline for Okta Syslogs for a business use case.
+ 
+
+The integration guide here presents an end-end solution for converting Okta Syslogs events into OCSF schema.
+The CloudFormation script provided as part of the documentation helps customers focus on the core transformation logic and not worry about integrating different AWS services. 
+Use AWS Lambda code as an example/baseline for performing an OCSF   transformation. AWS Lambda shows how to read and parse Okta’s Syslog event and map it to various categories of OCSF schema. 
+The DataCatalog resource in the CloudFormation script maintains a table schema. Please make sure the data catalog schema and JSON output from AWS Lambda match - any discrepancy between these two artifacts could result in an error. 
+
+
+### Extending/re-using the solution for other security events?
+
+As highlighted, the event considered for this post is successful user authentication in the Okta portal. If you want to extend this to other events, for example, user assignment, user un-assignment, etc. Please follow these steps: 
+Get a sample of the Syslogs event Link 
+Look at the OCSF Schema Link, and identify where the Okta event belongs. 
+Perform the data mapping exercise and map fields between Okta SysLog events to OCSF Schema. (Please refer to the current AWS Lambda for the current mapping)
+Modify the AWS Lambda function for the mapping defined in #3
+In the CloudFormation, you will need to determine the rule which captures Okta’s Syslog event. Update/Add the event rule in “AWS::Events::Rule” resource.
+For consistent and error-free transformation, update the glue data catalog schema from the CloudFormation to be compatible with AWS Lambda output.
+
+## How to map between the Okta System Log events and OCSF schema attributes ?
+
+As discussed in the above use case, the security event we are considering is “Successful Authentication.” As per the current OCSF schema, the event falls under the Audit Activity category. 
+
+Once a targeted event category is identified, refer to the OCSF schema information here OCSF schema. You will notice Authentication [3002] as a category; click it. It will point to the targeted schema, values, descriptions, etc. 
+
+You can start mapping the Okta Syslog event attribute to the destination schema. Here is a sample of mapping between Okta’s Syslog event and OCSF schema. 
+
+<b> <i> Please use the following mapping table as a reference only for schema mapping activity. Always check the latest OCSF schema when converting Okta System logs to OCSF schema. </i></b> 
+
+
+| Destination - OCSF Format        | Source - Okta system Log           | 
+| ------------- |:-------------:|  
+| activity_name     |  Check if "user.authentication" (Event Type) is present in data->detail->eventType | 
+| activity_id     |  Based on activity value (from row 1) select the activity_id value |   
+| auth_protocol | detail - > authenticationContext-> authenticationProvider| 
+| auth_protocol_id | Get value from the OCSF schema column description |
+| category_name | Audit Activity events. ( For Login Activity) |
+| category_uid | Get value from the OCSF schema column description page |
+| class_name | Type of event category |
+| class_uid | Based on class_name value (from row 9) select the class_uid value |
+| is_cleartext | If Authentication protocol from row #5 is not FTP /TELNET value will be FALSE |
+| dst_endpoint |  hostname: detail ->debugContext->debugData->requestUri : ip -> "" , instance_uid -> "" ,interface_id -> "",svc_name -> detail ->debugContext->debugData->url |
+| dst_user | detail->actor ->alternateID |
+| enrichments | detail ->target |
+| time | detail ->time |
+| logon_type | detail->transaction->type |
+| logon_type_id | detail->transaction->type ==Web then -1 |
+| message | detail-> displayMessage |
+| ref_time | time |
+| profiles | detail->actor->alternateId |
+| session_uid | detail-> authenticationContext-> externalSessionID |
+| severity | detail->info |
+| severity_id | detail->info Prase for int values based on OCSF schema|
+| src_endpoint | hostname: detail ->debugContext->debugData->requestUri,ip -> detail->client->userAgebt->IP, interface_id - > detail->client->userAgebt->device |
+| user | detail->actor->type,detail->actor-> displayname , detail-> actor- > alternateID |
+| status | detail-> Outcome |
+| status_detail | detail-> Outcome |
+| status_id | detail-> Outcome Parse and int |
+| type_name | Check if "user.authentication" (Event Type) is present in data['detail']['eventType'] |
+| unmapped  | JSON array ; values that are not mapped and are important for analysis purposes |
+| user user | detail-> target[0]->alternateID |
+| metadata  | data['time'] ,data['version'] |
+
+
+## Troubleshooting steps
+
+### I don’t see the parquet files created in a designated S3 bucket
+Verify if you see metrics for Event Rules invocations and TriggeredRules. Go to Amazon EventBridge Console, expand the left side panel, and select Rules under Events category. Select Event Bus for Okta and select event rule created for Amazon Security Lake.
+
+![image](https://user-images.githubusercontent.com/2838125/202004366-52ee9ef7-68bc-4056-bef2-193f06c98c06.png)
+
+
+Go to the Monitoring tab and view the metrics for Invocations and TriggeredRules.
+
+![image](https://user-images.githubusercontent.com/2838125/202004408-eb119f42-a597-41c0-a001-9ee203fcad36.png)
+
+Invocations metric shows the number of times a rule invokes a target in response to an event. If you do not see metrics for Invocations, there could be an issue with Okta sending Event data to the EventBridge. Verify your EventBridge connection at Okta and open a support case with Okta if required.
+TriggeredRules metric shows the number of rules that have run and matched with any event. If you do not see metrics for TriggeredRules, there could be an issue with the EventBridge Target which is the Kinesis Firehose Delivery Stream. Verify if this configured correctly and open a support case with AWS if required.
+
+Validate the Event rule configured on the EventBridge service. Check the pattern of the event and validate the source of the event. If an event bridge rule doesn't match the source event, the log event is ignored, and the rest of the data pipeline will fail.
+
+
+
+### My EventRule is working correctly, but I don’t see a parquet file?
+To verify if the data is received in EventBridge, you can create a rule to send the Okta Syslog events to different target destinations like Amazon SQS queue. Follow the steps below:
+* Click a SQS queue by following the steps in the AWS documentation.
+* Go to Amazon EventBridge AWS console. Select Rules and select the Event bus for Okta.
+* Click Create Rule. Enter a name for the Rule and description. Click Next.
+* Keep the Event Source as AWS events or EventBridge partner events. Check Sample event as EventBridge partner events
+![image](https://user-images.githubusercontent.com/2838125/202004659-6269e980-735c-40a1-b918-22acb4c6fdef.png)
+
+* Scroll down and under Event pattern, select Event source as EventBridge partners, Partner as Okta, and Event type as All Events
+![image](https://user-images.githubusercontent.com/2838125/202004770-5813e975-64ee-4ea4-b30a-104320450bac.png)
+
+* Click Next and under Target 1, check Target types as AWS service, Select a target as SQS queue, and select the SQS queue you have created earlier.
+![image](https://user-images.githubusercontent.com/2838125/202004843-b770294f-165f-4989-92cf-9c2e39cdf7de.png)
+
+* Click Next. Add any tags if required and Click Next. Review all the settings and click Create Rule.
+* Log in to Okta session and verify in the SQS queue if you are receiving event data. Follow the steps described in this AWS documentation to receive and verify the data in AWS console
+
+
+### Errors in processing records 
+
+For any issues with parquet file conversion or lambda errors, a file will be written in S3 bucket in a processing-failed folder. View the error using Query with the S3 select option as shown below.
+
+![image](https://user-images.githubusercontent.com/2838125/202005086-5605553d-320f-4458-816b-592ff8cb86d0.png)
+
+Choose the following option to retrieve log data. 
+
+![image](https://user-images.githubusercontent.com/2838125/202005167-a438bbf6-0dcf-41f4-95e2-5b119f550610.png)
+
+
+### If you see an error message like “The schema is invalid” or “Error parsing the schema during the OCSF conversion”
+
+Glue data catalog schema should be in sync with the JSON data (output from the lambda function). Please compare the Glue schema vs. JSON data and make the necessary changes accordingly. 
+
---
+++ app.py
@@ -0,0 +1,383 @@
+import base64
+from datetime import datetime
+import json
+
+
+def lambda_handler(event, context):
+    """
+    Purpose of this Lambda function is to provide a general guidance on converting Okta syslogs into OCSF format.
+    This Lambda function ONLY CONSIDERS : SUCCESSFUL AUTHENTICATION EVENT FROM OKTA SYS LOG
+    For different syslog event categories lambda should be enhanced/modified as per the OCSF schema and Sys Log event attributes
+    Parameters
+    ----------
+    event: event Object represents Okta SysLog event
+
+        Event doc: https://developer.okta.com/docs/reference/api/system-log/#example-logevent-object
+
+    Returns
+    ------
+    Output Format: Returning Okta Sys log event into an OCSF JSON format
+
+        To Understand OCSF Data Format: https://schema.ocsf.io/classes/authentication?extensions=
+    """
+    output = []
+    # Access Record Data from Syslog event
+    for record in event['records']:
+        data = base64.b64decode(record['data'])
+        data = json.loads(data.decode('utf8'))
+        # Invoke Transform Data to perform OCSF conversion
+        result = tranform_data(data)
+        # Add Dynamic Partioning for S3 buckets
+        format = "%Y-%m-%dT%H:%M:%S.%fZ"
+        date_input = data['detail']['published']
+
+        datetime1 = datetime.strptime(date_input, format)
+        partitionKeys = {}
+        partitionKeys["source"] = 'OktaEventSource'
+        partitionKeys["region"] = context.invoked_function_arn.split(":")[3]
+        partitionKeys["AWS_account"] = context.invoked_function_arn.split(":")[4]
+        partitionKeys["eventhour"] = datetime1.strftime("%Y%m%d%H")
+
+        # Reformats the output in a base64 encoded format.OCSF JSON Output will be used by Firehose datastream and AWS Glue Schema
+        output_record = {
+            'recordId': record['recordId'],  # is this the problem? I used sequenceNumber, it is not right.
+            'result': 'Ok',
+            'data': base64.b64encode(json.dumps(result, separators=(',', ':')).encode('utf-8') + b'\n').decode(
+                'utf-8'),
+            'metadata': {'partitionKeys': partitionKeys}
+        }
+        output.append(output_record)
+    print("JSON Output base64 Encoded format:")
+    print(output)
+    return {'records': output}
+
+
+def get_activity_details(activity_info):
+    """
+    Function captures an Activity Name that is logged by Okta Syslog
+    Parameters
+    ----------
+    activity_info: Activity Info captured by Okta system Log
+
+    Returns
+    ------
+    activity: Name of the activity
+    activity_id: Identifier for the activity
+    """
+    # Based on the OCSF schema definition, Successful Athentication is described as "unknown"
+    # Activity Value will change based on a type of event you want to capture
+    activity = "Unknown"
+    # Use Activity ID associated with an activity
+    activity_id = 0
+    # Check if User Authentication is part of the activity Info
+    if "user.authentication" in activity_info:
+        activity = 'Logon'
+        activity_id = 1
+    return activity, activity_id
+
+
+def get_auth_protocol(auth_provider_detail):
+    """
+    Function captures an authentication protocol reported by the event source.
+    Parameters
+    ----------
+    auth_provider_detail: Contains the identity of an actor using the credentials provided to it
+
+    Returns
+    ------
+    auth_protocol: Name of the activity
+    auth_protocol: Identifier for the activity
+    """
+    auth_protocol = "Unknown"
+    auth_protocol_id = 0
+    # Check if FACTOR is part of the activity Info
+    # this can be extended to various scenarios and use cases
+    if "FACTOR" in auth_provider_detail:
+        auth_protocol = "Other / MFA"
+        auth_protocol_id = 1
+    return auth_protocol, auth_protocol_id
+
+
+def get_audit_category(event_type):
+    """
+    Function captures the event category name for an event logged by Okta
+    get_audit_category function is dedicated for all the Audit Activity Events
+    This function can be enhanced as more events are included
+    Returns
+    ------
+    category_name: Name of the event category
+    category_uid: Category unique identifier for the activity
+    """
+    # The event category name, for Successful Authentication , category name and category_uid are selected based on the OCSF schema
+    category_name = "Unknown"
+    category_uid = 0
+    if "user.authentication" in event_type:
+        category_name = 'Audit Activity events'
+        category_uid = 3
+    return category_name, category_uid
+
+
+def get_event_class():
+    """
+    Function captures an event class
+
+    Returns
+    ------
+    class_name: Name of the event class
+    class_uid: Class unique identifier for the activity
+    """
+    class_name = "Authentication Audit"
+    class_uid = 3002
+    return class_name, class_uid
+
+
+def get_clear_text_value(auth_protocol):
+    """
+    Function checks if credentials were passed in clear text.
+    Parameters
+    ----------
+    auth_protocol: Contains the metadata about the authentication
+    Returns
+    ------
+      Returns the boolean value
+    """
+    # check if protocol is either FTP or Telnet
+    return auth_protocol != 'FTP' and auth_protocol != 'TELNET'
+
+
+def get_destination_endpoint(destination_endpoint):
+    """
+    Function finds the endpoint for which Authn was targeted
+    Parameters
+    ----------
+    destination_endpoint: Contains the metadata about the endpoint for which AuthN was targeted
+
+    Returns
+    ------
+    detination_details: Returns the destination endpoint as a dictionary
+    """
+    # Create a JSON object in OCSF format
+    detination_details = {"hostname": destination_endpoint['requestUri'],
+                          "ip": "",
+                          "instance_uid": "",
+                          "interface_id": "",
+                          "svc_name": destination_endpoint['url']}
+    return detination_details
+
+
+def get_logon_type(login_transaction):
+    """
+    Function finds the type of the login based on the event source
+    Parameters
+    ----------
+    login_transaction: Contains the metadata about the endpoint for which AuthN was targeted
+
+    Returns
+    ------
+    logon_type: Returns the boolean value based on the event
+    logon_type_id: Returns the logon id
+    """
+    # Capture the login transaction
+    logon_type = login_transaction['type']
+    # If WEB is not in logon_type return a normalized value
+    logon_type_id = - 1 if "WEB" in logon_type else 0
+
+    return logon_type, logon_type_id
+
+
+def get_severity(severity):
+    """
+    Function to find the log severity
+    Parameters
+    ----------
+    severity: Details about the event severity
+
+    Returns
+    ------
+    severity: Returns the event severity
+    severity_id: Returns event severity  id
+    """
+    # If the event severity is INFO assign value as 1
+    severity_id = 1 if "INFO" in severity else 0
+
+    return severity, severity_id
+
+
+def get_src_endpoint(data):
+    """
+    Function to find the endpoint where authentication is requested
+    Parameters
+    ----------
+    data: Details about the event
+
+    Returns
+    ------
+    src_end_point: Returns the src endpoint
+    """
+    # Create JSON formatted string compatible with OCSF schema
+    return {
+        "hostname": data['debugContext']['debugData']['requestUri'],
+        "ip ": data['client']['ipAddress'],
+        "interface_id": data['client']['device']
+    }
+
+
+def get_src_user(data):
+    """
+    Function to find the endpoint where authentication is requested
+    Parameters
+    ----------
+    data: Get existing user data
+
+    Returns
+    ------
+    src_user: Returns the user information
+    """
+    # Create JSON formatted string compatible with OCSF schema
+    return {
+        'type': data['actor']['type'],
+        'name': data['actor']['displayName'],
+        'email_addr': data['actor']['alternateId']
+    }
+
+
+def get_status_details(data):
+    """
+    Function to find the endpoint where authentication is requested
+    Parameters
+    ----------
+    data: Get existing user data
+
+    Returns
+    ------
+    status_result: Returns the event status
+    status_code: Returns the event status code
+    status_detail: Details about authentication Request
+    status_id: Normalized ID for the status
+    """
+    status_result = data['outcome']['result']
+    status_code = 'N/A'
+    status_detail = ''
+    status_id = -1
+    if "SUCCESS" in status_result:
+        status_detail = "LOGON_USER_INITIATED"
+        status_id = 1
+    return status_result, status_code, status_detail, status_id
+
+
+def get_type_category(eventType):
+    """
+    Function captures the event type for an event logged by Okta
+    get_audit_category function is dedicated for all the Audit Activity Types
+    This function can be enhanced as more events are included
+    Returns
+    ------
+    type_name: Name of the event Type
+    type_uid: Type unique identifier for the activity
+    """
+    # The event category name, for Successful Authentication , category name and category_uid are selected based on
+    # the OCSF schema
+    type_uid = 0
+    type_name = "Unknown"
+    if "user.authentication" in eventType:
+        type_name = 'Authentication Audit: Logon'
+        type_uid = 300201
+    return type_uid, type_name
+
+def get_metadata(original_time,version):
+    """
+    Function captures the metadata about the event type for an event logged by Okta
+    get_metadata function is dedicated for capturing the Metadata Object Type
+    This function can be be enhanced as more events are included
+    Returns
+    ------
+    metadata: Metadata Object is returned
+    """
+    # Create JSON formatted string compatible with OCSF schema
+    return {
+        'original_time': original_time,
+        'product': {
+                    'vendor_name':'Okta',
+                    'name': 'Okta System Log'
+                    },
+        'version': version
+    }
+
+
+def tranform_data(data):
+    # get activity details based on the eventType that is published
+    activity, activity_id = get_activity_details(data['detail']['eventType'])
+    # get the authentication protocol used to create the user session.
+    auth_protocol, auth_protocol_id = get_auth_protocol(
+        data['detail']['authenticationContext']['authenticationProvider'])
+    # get the event category name,
+    category_name, category_uid = get_audit_category(data['detail']['eventType'])
+    # get the event class name
+    class_name, class_uid = get_event_class()
+    # check if whether the credentials were passed in clear text.
+    is_cleartext = get_clear_text_value(auth_protocol)
+    # get the destination endpoint for which the authentication was targeted.
+    dst_endpoint = get_destination_endpoint(data['detail']['debugContext']['debugData'])
+    # get user details and account type used for authentication
+    dst_user = data['detail']['actor']['alternateId']
+    # get additional additional information which is critical for the event but doesn't fall under OCSF schema
+    enrichments = data['detail']['target']
+    # get time of the event
+    _time = data['time']
+    # get type of the logon
+    logon_type, logon_type_id = get_logon_type(data['detail']['transaction'])
+    # get the description of the message
+    display_message = data['detail']['displayMessage']
+    # get the original event as reported
+    ref_time = data['time']
+    # get userID value
+    profile = data['detail']['actor']['alternateId']
+    # get the Session UID value
+    session_uid = data['detail']['authenticationContext']['externalSessionId']
+    # get the log severity of the event
+    severity, severity_id = get_severity(data['detail']['severity'])
+    # get the endpoint from which the authentication was requested.
+    src_endpoint = get_src_endpoint(data['detail'])
+    # get existing user from which an activity was initiated.
+    src_user = get_src_user(data['detail'])
+    # get event status details in OCSF format
+    status, status_code, status_detail, status_id = get_status_details(data['detail'])
+    # get event type details in OCSF format
+    type_uid, type_name = get_type_category(data['detail']['eventType'])
+    # get metadata about the event type in OCSF format
+    metadata= get_metadata(data['time'],data['version'])
+    # Assemeble the JSON string in OCSF format
+    json_data = {
+        'activity_name': activity,
+        'activity_id': activity_id,
+        'auth_protocol': auth_protocol,
+        'auth_protocol_id': auth_protocol_id,
+        'category_name': category_name,
+        'category_uid': category_uid,
+        'class_name': class_name,
+        'class_uid': class_uid,
+        'is_cleartext': is_cleartext,
+        'dst_endpoint': dst_endpoint,
+        'dst_user': dst_user,
+        'enrichments': enrichments,
+        'time': _time,
+        'logon_type': logon_type,
+        'logon_type_id': logon_type_id,
+        'displayMessage': display_message,
+        'ref_time': ref_time,
+        'profile': profile,
+        'session_uid': session_uid,
+        'severity': severity,
+        'severity_id': severity_id,
+        'src_endpoint': src_endpoint,
+        'user': src_user,
+        'status': status,
+        'status_code': status_code,
+        'status_detail': status_detail,
+        'status_id': status_id,
+        'type_uid': type_uid,
+        'type_name': type_name,
+        'metadata': metadata
+    }
+    # Return the JSON String
+    return json_data
